62f80973d7f7de60a8b8c7f702843aa97ae26823
Somov Evgeniy <evgeniy.somov@ilogos.biz>
2020-01-22 11:11:59 +0300

    Вывод HAR данных в локальном теста

--- Java/src/test/java/Tests/LocalhostTest1.java
+++ Java/src/test/java/Tests/LocalhostTest1.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.Assert;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -16,8 +17,8 @@ import net.lightbody.bmp.core.har.Har;
 
 public class LocalhostTest1
 {
-    private static WebDriver driver;
-    private static BrowserMobProxy proxy;
+    public static WebDriver driver;
+    public static BrowserMobProxy proxy;
 
     @BeforeClass
     public static void setup()
@@ -27,6 +28,7 @@ public class LocalhostTest1
 
         // старт прокси
         proxy = new BrowserMobProxyServer();
+        proxy.setTrustAllServers(true);
         proxy.start();
 
         // получить объект Selenium
@@ -35,10 +37,11 @@ public class LocalhostTest1
         // настройка для драйвера
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability(CapabilityType.PROXY, seleniumProxy);
+        ChromeOptions options = new ChromeOptions();
+        options.merge(capabilities);
 
         // создание драйвера
-        driver = new ChromeDriver(capabilities);
-        driver.manage().window().maximize();
+        driver = new ChromeDriver(options);
 
         // включить более детальный захват HAR
         proxy.newHar("localhost:8888");
@@ -48,17 +51,33 @@ public class LocalhostTest1
     public void testSearch()
     {
         driver.get("http://localhost:8888/");
+
+        WebElement username = driver.findElement(By.name("username"));
+        username.sendKeys("admin");
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("123");;
+
         try {
-            Thread.sleep(10000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
+        WebElement button = driver.findElement(By.name("button"));
+        button.click();
+
         // получить данные HAR
         Har har = proxy.getHar();
-        System.out.println(har);
+        System.out.println("HAR: " + har.getLog().getVersion());
+        for (int i = 0; i < har.getLog().getEntries().size(); i++)
+        {
+            String link = har.getLog().getEntries().get(i).getRequest().getUrl();
+            System.out.println("HAR LINK: " + link);
+        }
 
-        Assert.assertTrue(true);
+        String result = driver.findElement(By.id("result")).getText();
+        System.out.println("RESULT: " + result);
+        Assert.assertEquals("Correct", result);
     }
 
     @AfterClass
